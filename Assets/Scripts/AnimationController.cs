83047411f50522819ac527591830cc45831c496d
Arindom Das <arindom@minigamelab.com>
2022-12-02 02:29:32 +0600

    first 10 second is working fine

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class AnimationController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject humanObj,
+        butterflyObj,
+        monsterObj;
+
     private void Start()
     {
+        ManageButterfly();
+    }
 
+    private void ManageButterfly()
+    {
+        DOTween.Sequence().AppendInterval(2.8f)
+            .AppendCallback(() => butterflyObj.SetActive(true))
+            .AppendInterval(3.5f)
+            .AppendCallback(() => butterflyObj.SetActive(false));
     }
 }
